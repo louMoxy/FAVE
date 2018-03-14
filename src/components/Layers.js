4dc75e6f6712a978a20f22b353296d6dbe9d5776
louMoxy <louisemoxy@gmail.com>
2018-03-14 06:54:50 +0000

    Add dummy data to generate markers

--- src/components/Layers.js
+++ src/components/Layers.js
@@ -2,15 +2,19 @@ import React from "react";
 import { Layer, Feature } from "react-mapbox-gl";
 
 const circleStyle = {
-  "circle-radius": 35,
+  "circle-radius": 8,
   "circle-color": "#E54E52",
   "circle-opacity": 1
 };
 export class Layers extends React.Component {
   render() {
+    const data = require("../data/dummy-data.json");
+    const layers = Object.values(data).map(val => {
+      return <Feature coordinates={val.location} />;
+    });
     return (
       <Layer type="circle" paint={circleStyle}>
-        <Feature coordinates={[-3.533518, 50.46349]} />
+        {layers}
       </Layer>
     );
   }
