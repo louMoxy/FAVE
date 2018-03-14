a050691b8c2ac6ded8cbdf3fb070056a70b91d52
louMoxy <louisemoxy@gmail.com>
2018-03-14 07:22:58 +0000

    Add map popup and use one component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,51 @@
 import React from "react";
-import ReactMapboxGl from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Feature, Popup } from "react-mapbox-gl";
 
-export const Map = ReactMapboxGl({
+const MapBox = ReactMapboxGl({
   accessToken:
     "pk.eyJ1IjoibG1veGhheSIsImEiOiJjamVvcW1uMW0xbHkwMzNyd2Y5YzNkMHVvIn0.7SmoGVAYH5kIkqPY7NIV6g"
 });
+
+const circleStyle = {
+  "circle-radius": 8,
+  "circle-color": "#E54E52",
+  "circle-opacity": 1
+};
+export class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      places: {},
+      selectedPlace: null
+    };
+  }
+
+  componentWillMount() {
+    const data = require("../data/dummy-data.json");
+    Object.values(data).map(val => {
+      return <Feature coordinates={val.location} />;
+    });
+    this.setState({ places: data });
+  }
+
+  render() {
+    const { places, selectedPlace } = this.state;
+    return (
+      <MapBox
+        style="mapbox://styles/mapbox/streets-v8"
+        center={[-3.533518, 50.46349]}
+      >
+        <Layer type="circle" paint={circleStyle}>
+          {Object.values(places).map((place, index) => {
+            return <Feature coordinates={place.location} key={place.name} />;
+          })}
+        </Layer>
+        {selectedPlace && (
+          <Popup key={selectedPlace.name} coordinates={selectedPlace.location}>
+            <h1>Popup</h1>
+          </Popup>
+        )}
+      </MapBox>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 import { Map } from "./components/Map";
-import { Layers } from "./components/Layers";
 import "./styles/app.css";
 
 class App extends React.Component {
@@ -9,12 +8,7 @@ class App extends React.Component {
     return (
       <div className="app">
         <div>Side bar </div>
-        <Map
-          style="mapbox://styles/mapbox/streets-v8"
-          center={[-3.533518, 50.46349]}
-        >
-          <Layers />
-        </Map>
+        <Map />
       </div>
     );
   }
